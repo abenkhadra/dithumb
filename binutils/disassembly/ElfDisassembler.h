be1bc05d46e2b5d266dc9ff270c025a41b107b04
Ammar Ben Khadra <ammarbenkhadra@gmail.com>
2015-10-12 10:24:46 +0200

    Finish sample disassembler
    Finished a very basic version of the disassembler. More serious work should take-off from here.

---
+++ binutils/disassembly/ElfDisassembler.h
@@ -0,0 +1,71 @@
+//===------------------------------------------------------------*- C++ -*-===//
+//
+// This file is distributed under the MIT License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+// 
+// Copyright (c) 2015 Technical University of Kaiserslautern.
+// Created by M. Ammar Ben Khadra.
+
+#pragma once
+#include "elf/elf++.hh"
+#include <capstone/capstone.h>
+
+namespace disasm {
+
+struct CapstoneConfig final{
+public:
+    CapstoneConfig():
+        arch_type{CS_ARCH_ARM},
+        mode{CS_MODE_THUMB},
+        details{true}{
+    }
+    CapstoneConfig(const CapstoneConfig& src) = default;
+    CapstoneConfig &operator=(const CapstoneConfig& src) = default;
+
+    cs_arch arch_type;
+    cs_mode mode;
+    bool details;
+};
+/**
+ * ElfDisassembler
+ * This class is internally reference counted and efficiently
+ * copyable.
+ */
+class ElfDisassembler {
+public:
+    /**
+     * Construct a Elf Disassembler that is initially not valid.  Calling
+     * methods other than valid on this results in undefined behavior.
+     */
+    ElfDisassembler();
+    /**
+     * Prepares input file for disassembly.
+     * Pre-condition: file is a valid ELF file.
+     */
+    ElfDisassembler(const elf::elf &elf_obj);
+    ElfDisassembler(const elf::elf &elf_obj, const CapstoneConfig &config);
+    virtual ~ElfDisassembler() = default;
+    ElfDisassembler(const ElfDisassembler &src) = default;
+    ElfDisassembler &operator=(const ElfDisassembler &src) = default;
+    ElfDisassembler(ElfDisassembler &&src) = default;
+
+    bool valid() const { return m_valid; }
+    void disassembleCode() const;
+    void disassembleCode2() const;
+    void disassembleSectionbyName(std::string& sec_name) const;
+    void print_string_hex(unsigned char *str, size_t len) const;
+
+private:
+    void disassembleSection(const elf::section &sec, const csh *handle) const;
+    void initializeCapstone(csh *handle) const;
+
+private:
+    bool m_valid;
+    const elf::elf* m_elf_obj;
+    CapstoneConfig m_config;
+};
+}
+
+
+
---
+++ binutils/disassembly/ElfMemImage.cpp
@@ -0,0 +1,74 @@
+//===------------------------------------------------------------*- C++ -*-===//
+//
+// This file is distributed under the MIT License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+// 
+// Copyright (c) 2015 Technical University of Kaiserslautern.
+// Created by M. Ammar Ben Khadra.
+
+#include "ElfMemImage.h"
+#include <algorithm>
+#include <assert.h>
+
+namespace disasm {
+
+using namespace std;
+
+
+ElfMemImage::ElfMemImage(const elf::elf &elf_file):
+    m_elf_file(elf_file),
+    m_valid{true}{
+
+    for (const auto &sec : elf_file.sections()) {
+        if (sec.is_alloc()) {
+            if (sec.is_writable()) {
+                m_write_sec.push_back(&sec);
+            }else if (sec.is_exec()) {
+                m_exec_sec.push_back(&sec);
+            }else{
+                m_read_only_sec.push_back(&sec);
+            }
+
+        }
+
+    }
+}
+
+
+const void *
+ElfMemImage::Load(off_t vaddr, size_t size){
+//    if (vaddr < pimpl->m_base_vaddr || size > pimpl->m_size )
+//        throw range_error("virtual address out of range");
+//    return (const char*) pimpl->getPAddr(vaddr) ;
+    return nullptr;
+}
+
+
+
+const vector<elf::section*>
+ElfMemImage::getReadOnlySections() const{
+//    std::vector<elf::section> result;
+//
+//    assert(m_elf_file.sections().size() != 0);
+//
+//    std::copy_if(m_elf_file.sections().begin(),
+//                 m_elf_file.sections().end(),
+//                 std::back_inserter(result),
+//                 [](const elf::section &sec) { return sec.is_alloc()
+//                     &&(!(sec.is_writable()||sec.is_exec())); });
+
+    return m_read_only_sec;
+}
+
+const vector<elf::section*>
+ElfMemImage::getWriteSections() const {
+    return m_write_sec;
+}
+
+const vector<elf::section*>
+ElfMemImage::getExecSections() const {
+    return m_exec_sec;
+}
+
+}
