d4dea0ef1d2aad28b8778a43b4479863bebf6dc8
Ammar Ben Khadra <ammarbenkhadra@gmail.com>
2016-03-19 12:57:14 +0100

    Simplify output
    commented out printing of instruction details for cleaner output

--- modules/disasm/ElfDisassembler.cpp
+++ modules/disasm/ElfDisassembler.cpp
@@ -205,35 +205,35 @@ void ElfDisassembler::prettyPrintInst(const csh &handle, cs_insn *inst) const {
            inst->id, cs_insn_name(handle, inst->id));
 
     // print implicit registers used by this instruction
-    detail = inst->detail;
-
-    if (detail == NULL) return;
-
-    if (detail->regs_read_count > 0) {
-        printf("\tImplicit registers read: ");
-        for (n = 0; n < detail->regs_read_count; n++) {
-            printf("%s ", cs_reg_name(handle, detail->regs_read[n]));
-        }
-        printf("\n");
-    }
-
-    // print implicit registers modified by this instruction
-    if (detail->regs_write_count > 0) {
-        printf("\tImplicit registers modified: ");
-        for (n = 0; n < detail->regs_write_count; n++) {
-            printf("%s ", cs_reg_name(handle, detail->regs_write[n]));
-        }
-        printf("\n");
-    }
-
-    // print the groups this instruction belong to
-    if (detail->groups_count > 0) {
-        printf("\tThis instruction belongs to groups: ");
-        for (n = 0; n < detail->groups_count; n++) {
-            printf("%s ", cs_group_name(handle, detail->groups[n]));
-        }
-        printf("\n");
-    }
+//    detail = inst->detail;
+//
+//    if (detail == NULL) return;
+//
+//    if (detail->regs_read_count > 0) {
+//        printf("\tImplicit registers read: ");
+//        for (n = 0; n < detail->regs_read_count; n++) {
+//            printf("%s ", cs_reg_name(handle, detail->regs_read[n]));
+//        }
+//        printf("\n");
+//    }
+//
+//    // print implicit registers modified by this instruction
+//    if (detail->regs_write_count > 0) {
+//        printf("\tImplicit registers modified: ");
+//        for (n = 0; n < detail->regs_write_count; n++) {
+//            printf("%s ", cs_reg_name(handle, detail->regs_write[n]));
+//        }
+//        printf("\n");
+//    }
+//
+//    // print the groups this instruction belong to
+//    if (detail->groups_count > 0) {
+//        printf("\tThis instruction belongs to groups: ");
+//        for (n = 0; n < detail->groups_count; n++) {
+//            printf("%s ", cs_group_name(handle, detail->groups[n]));
+//        }
+//        printf("\n");
+//    }
     if (isBranch(inst)) {
         printf("End of Basic Block.\n");
         printf("***********************************\n");
